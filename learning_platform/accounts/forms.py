e1824fbd8f8dd49dc07c3485545ace2b8b89561a
toprak gungor <toprakgungor02@gmail.com>
2024-08-05 16:31:05 +0300

    register page fixed

--- learning_platform/accounts/forms.py
+++ learning_platform/accounts/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    user_type = forms.ChoiceField(choices=CustomUser.USER_TYPE_CHOICES)
+    email = forms.EmailField(required=True)
 
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('user_type',)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- learning_platform/accounts/urls.py
+++ learning_platform/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import CustomUserCreationForm
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('register/', CreateView.as_view(template_name='accounts/register.html', form_class=CustomUserCreationForm), name='register'),
 ]
