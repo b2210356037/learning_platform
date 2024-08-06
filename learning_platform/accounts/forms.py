b5c1bb31359d64527d291cc06512386cbe86c503
toprak gungor <toprakgungor02@gmail.com>
2024-08-06 10:40:10 +0300

    chore: Register CustomUser model in admin site with UserAdmin

--- learning_platform/accounts/forms.py
+++ learning_platform/accounts/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- learning_platform/accounts/views.py
+++ learning_platform/accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def logout_view(request):
     logout(request)
