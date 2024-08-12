68dd355ca7c32160990500e307640362c0ee2904
İclal Gül <iclalgulll67@gmail.com>
2024-08-12 14:53:25 +0300

    course detail fixed

--- learning_platform/courses/forms.py
+++ learning_platform/courses/forms.py
@@ -10,3 +10,8 @@ class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = ['title', 'content', 'order', 'video']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.TextInput(attrs={'class': 'form-control'}),
+            'video': forms.TextInput(attrs={'class': 'form-control'}),
+        }
